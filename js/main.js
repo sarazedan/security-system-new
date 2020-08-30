253f6ec5eef110e6656cf4e050b20d8bb16ee7fe
sarazedan <sarazedan369@gmail.com>
2020-08-30 11:07:21 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+$(function(){
+	
+	 //featured work shuflle
+    $('.gallery ul li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+	 
+	    //console.log($(this).data('class'));
+		if($(this).data('class')==='all'){
+			$('.shuffle-imgs .col-sm').css('opacity',1);
+		}
+		else{
+		
+			$('.shuffle-imgs .col-sm').css('opacity','.09');
+			$($(this).data('class')).parent().css('opacity',1);
+			
+		}
+			
+		});
+			 $(window).scroll(function(){
+		 
+		 var navbar=$('.navbar');
+		 if($(window).scrollTop() >= navbar.height()){
+			 
+			 navbar.addClass('scrolled');
+		 }
+		 else{
+			 
+			 navbar.removeClass('scrolled');
+			 
+		 }
+	 
+	 });
+
+
+
+});
+
+
 /* =================================
 ------------------------------------
 	Labs - Design Studio
@@ -272,4 +320,6 @@ function progressCircle() {
 	progressCircle();
 
 })(jQuery);
+
+
 
